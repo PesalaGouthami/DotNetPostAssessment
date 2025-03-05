41583fb7520b3872913936f1c25c83f0eeed4725
farhan-ans-18 <farhan.ansari@tavant.com>
2025-03-05 17:37:56 +0530

    Delete ResumeTrackingSystem/ResumeApiConsume directory

--- ResumeTrackingSystem/ResumeApiConsume/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ResumeApiConsume.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
